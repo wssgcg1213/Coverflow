f3e477127fe6fcacd789cdb83dfcd9e82c61c861
Liuchenling <wssgcg1213@qq.com>
2014-02-08 23:36:11 +0800

    add::移动端两指拖动手势可以翻动

--- js/flow.js
+++ js/flow.js
@@ -20,6 +20,23 @@ function flowInit () {
 			$(document).on("mousewheel", mouseScroll);
 		}, 500);
 	}
+    document.addEventListener('touchstart',touches,false);
+    document.addEventListener('touchend',touches,false);
+    function touches(ev){
+        if(ev.touches.length==1){
+            switch(ev.type){
+                case 'touchstart':
+                    window.tx = ev.touches[0].clientX;
+                    break;
+                case 'touchend':
+                    var temp = ev.changedTouches[0].clientX - window.tx;
+                    if(temp > 0){rightFlow()};
+                    if(temp < 0){leftFlow()};
+                    break;
+
+            }
+        }
+    }
 
 }
 
